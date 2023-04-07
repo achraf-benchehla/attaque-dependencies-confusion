eeb06d67a6472701e11c11747c680634a1322779
ACHRAF BENCHEHLA <103905618+achraf-benchehla@users.noreply.github.com>
2023-04-07 17:34:48 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def main():
+   print ("Hello World")
+
+if __name__=="__main__":
+   main()
+    
+    #  La première ligne "#!/usr/bin/python3" est appelée shebang et indique 
+  #    au système d'exploitation l'interpréteur à utiliser pour exécuter le script.
+  #    Dans ce cas, elle indique que le script doit être exécuté en utilisant Python 3.
+   #   Si vous lancez ce script directement sur une machine Linux, vous pouvez
+    #  le rendre exécutable avec la commande "chmod +x nom_du_script.py" et
+    #  l'exécuter en tapant "./nom_du_script.py".
+
+#  Le reste du code définit une fonction nommée "main"
+#  qui imprime "Hello World" sur la sortie standard. Ensuite,
+#  le code vérifie si le script est exécuté en tant que programme
+#  principal en vérifiant si "name" est égal à "main". Si c'est le cas,
+#  cela signifie que le script est exécuté directement plutôt que d'être
+#  importé en tant que module, et donc la fonction "main" est appelée
+#  pour lancer le script.
+    
