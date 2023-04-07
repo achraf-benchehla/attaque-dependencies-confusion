33c2f81b6ee919839df743c9134db65fce70f672
ACHRAF BENCHEHLA <103905618+achraf-benchehla@users.noreply.github.com>
2023-04-07 17:28:48 +0000

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# ce fichier peut etre vide 
+
+
+Le fichier "init.py" est un fichier spécial en Python qui est utilisé pour marquer un répertoire comme un package Python
+. Son rôle principal est de définir l'espace de noms du package et d'initialiser les variables et les fonctions qui doivent 
+être disponibles lorsqu'un utilisateur importe le package.
+
+Le contenu du fichier "init.py" peut varier selon le package et son but.
+Il peut contenir des définitions de classes, des fonctions, des variables,
+des imports de modules, ou même être vide. Lorsqu'un package est importé, Python
+exécute le code contenu dans le fichier "init.py" avant de rendre les éléments
+du package disponibles pour utilisation.
+
+En résumé, "init.py" est un fichier obligatoire pour marquer un répertoire 
+en tant que package Python, et il permet de définir l'espace de noms et
+d'initialiser les variables et fonctions qui sont nécessaires pour l'utilisation du package.
